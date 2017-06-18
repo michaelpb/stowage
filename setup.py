ddd389edf84af541720a1ac3032961cadbc85c3b
michaelb <michaelpb@gmail.com>
2017-06-17 22:39:15 -0700

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ history = ''
 
 setup(
     name='stowage',
-    version='0.1.6',
+    version='0.1.7',
     description='Stow-like application for keeping '
                 'your dot-files under version control',
     long_description=readme + '\n\n' + doclink + '\n\n' + history,
